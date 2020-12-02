1b55d126d08c77842140206437c1354ec9aef32f
jaumemy <jaume.montane@outlook.com>
2020-12-02 13:56:44 -0500

    Part 7

--- spacer/galaxies/views.py
+++ spacer/galaxies/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
+from galaxies.models import Galaxy, GalaxyMember
 
 # Create your views here.
+
+class CreateGalaxy(LoginRequiredMixin, generic.CreateView):
+    fields = ('name','description')
+    model = Galaxy
+
+class SingleGalaxy(generic.DetailView):
+    model = Galaxy
+
+class ListGalaxies(generic.ListView):
+    model = Galaxy

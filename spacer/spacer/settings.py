48d4e03352f427c4a9a920deda174690b1f4f845
jaumemy <jaume.montane@outlook.com>
2020-12-04 14:13:26 -0500

    Part 14 - Heroku 9

--- spacer/spacer/settings.py
+++ spacer/spacer/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = [Path.joinpath(BASE_DIR,"staticfiles")]
 
 STATIC_URL = '/static/'
 

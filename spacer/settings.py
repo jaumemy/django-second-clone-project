164d923a8a54ac2252f83f3ce4962390c7c3c73f
jaumemy <jaume.montane@outlook.com>
2021-02-08 18:40:15 -0500

    Fiex dj_database_url

--- spacer/settings.py
+++ spacer/settings.py
@@ -140,10 +140,3 @@ STATICFILES_DIRS = [Path.joinpath(BASE_DIR,"static")]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
-
-
-
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
